016d38507342d1bbe4ad7b006bee4dda3e327aeb
elielnogueira <elielguimaraesnogueira@gmail.com>
2021-01-03 17:49:30 -0300

    vai tudo

---
+++ kraticos-list-icons.php
@@ -0,0 +1,165 @@
+<?php 
+// - ICONES
+$sub_folder_img = "img";
+$sub_folder_icons = "icones";
+$size_icons = "20px";
+ 
+$icone_kraticons_logomarca = "<img src=\"icones/$sub_folder_img/logomarca.png\" width=\"$size_icons\">";
+
+$icone_kraticons_semfoto = "<img src=\"icones/$sub_folder_img/semfoto.png\" width=\"$size_icons\">";
+
+$icone_kraticons_success = "<img src=\"icones/$sub_folder_img/logomarca_success.png\" width=\"$size_icons\">";
+
+$icone_kraticons_fail = "<img src=\"icones/$sub_folder_img/logomarca_fail.png\" width=\"$size_icons\">";
+
+$icone_kraticons_suspended = "<img src=\"icones/$sub_folder_img/logomarca_suspended.png\" width=\"$size_icons\">";
+
+$icone_kraticons_nofoto = "<img src=\"icones/$sub_folder_img/semfoto.jpg\" width=\"$size_icons\">";
+
+
+
+$icone_welcome = "<img src=\"icones/$sub_folder_icons/welcome.png\" width=\"$size_icons\">";
+$icone_user_adm = "<img src=\"icones/$sub_folder_icons/user_admin.png\" width=\"$size_icons\">";
+$icone_id = "<img src=\"icones/$sub_folder_icons/id.png\" width=\"$size_icons\">";
+$icone_status = "<img src=\"icones/$sub_folder_icons/status.png\" width=\"$size_icons\">";
+$icone_person = "<img src=\"icones/$sub_folder_icons/person.png\" width=\"$size_icons\">";
+$icone_contract = "<img src=\"icones/$sub_folder_icons/contract.png\" width=\"$size_icons\">";
+$icone_password = "<img src=\"icones/$sub_folder_icons/password.png\" width=\"$size_icons\">";
+$icone_calendar = "<img src=\"icones/$sub_folder_icons/calendar.png\" width=\"$size_icons\">";
+$icone_mail = "<img src=\"icones/$sub_folder_icons/mail.png\" width=\"$size_icons\">";
+$icone_add = "<img src=\"icones/$sub_folder_icons/add.png\" width=\"$size_icons\">";
+$icone_remove = "<img src=\"icones/$sub_folder_icons/remove.png\" width=\"$size_icons\">";
+$icone_removew = "<img src=\"icones/$sub_folder_icons/removew.png\" width=\"$size_icons\">";
+$icone_save = "<img src=\"icones/$sub_folder_icons/save.png\" width=\"$size_icons\">";
+$icone_savew = "<img src=\"icones/$sub_folder_icons/savew.png\" width=\"$size_icons\">";
+$icone_edit = "<img src=\"icones/$sub_folder_icons/edit.png\" width=\"$size_icons\">";
+$icone_editw = "<img src=\"icones/$sub_folder_icons/editw.png\" width=\"$size_icons\">";
+$icone_opinion = "<img src=\"icones/$sub_folder_icons/opinion.png\" width=\"$size_icons\">";
+$icone_process = "<img src=\"icones/$sub_folder_icons/process.png\" width=\"$size_icons\">";
+$icone_whatsapp = "<img src=\"icones/$sub_folder_icons/whatsapp.png\" width=\"$size_icons\">";
+$icone_telephone = "<img src=\"icones/$sub_folder_icons/telephone.png\" width=\"$size_icons\">";
+$icone_cart = "<img src=\"icones/$sub_folder_icons/cart.png\" width=\"$size_icons\">";
+$icone_basket = "<img src=\"icones/$sub_folder_icons/basket.png\" width=\"$size_icons\">";
+$icone_locked = "<img src=\"icones/$sub_folder_icons/locked.png\" width=\"$size_icons\">";
+$icone_unlocked = "<img src=\"icones/$sub_folder_icons/unlocked.png\" width=\"$size_icons\">";
+$icone_unlock = "<img src=\"icones/$sub_folder_icons/unlock.png\" width=\"$size_icons\">";
+$icone_sale = "<img src=\"icones/$sub_folder_icons/sale.png\" width=\"$size_icons\">";
+$icone_buy = "<img src=\"icones/$sub_folder_icons/buy.png\" width=\"$size_icons\">";
+$icone_pay = "<img src=\"icones/$sub_folder_icons/pay.png\" width=\"$size_icons\">";
+$icone_paid = "<img src=\"icones/$sub_folder_icons/paid.png\" width=\"$size_icons\">";
+$icone_money = "<img src=\"icones/$sub_folder_icons/money.png\" width=\"$size_icons\">";
+$icone_payment = "<img src=\"icones/$sub_folder_icons/payment.png\" width=\"$size_icons\">";
+$icone_customers = "<img src=\"icones/$sub_folder_icons/customers.png\" width=\"$size_icons\">";
+$icone_administrators = "<img src=\"icones/$sub_folder_icons/administrators.png\" width=\"$size_icons\">";
+$icone_sucess = "<img src=\"icones/$sub_folder_icons/sucess.png\" width=\"$size_icons\">";
+$icone_fail = "<img src=\"icones/$sub_folder_icons/fail.png\" width=\"$size_icons\">";
+$icone_versus = "<img src=\"icones/$sub_folder_icons/versus.png\" width=\"$size_icons\">";
+$icone_minus = "<img src=\"icones/$sub_folder_icons/minus.png\" width=\"$size_icons\">";
+$icone_more = "<img src=\"icones/$sub_folder_icons/more.png\" width=\"$size_icons\">";
+$icone_heart = "<img src=\"icones/$sub_folder_icons/heart.png\" width=\"$size_icons\">";
+$icone_cancel = "<img src=\"icones/$sub_folder_icons/cancel.png\" width=\"$size_icons\">";
+$icone_delete = "<img src=\"icones/$sub_folder_icons/delete.png\" width=\"$size_icons\">";
+$icone_like = "<img src=\"icones/$sub_folder_icons/like.png\" width=\"$size_icons\">";
+$icone_dislike = "<img src=\"icones/$sub_folder_icons/dislike.png\" width=\"$size_icons\">";
+$icone_search = "<img src=\"icones/$sub_folder_icons/search.png\" width=\"$size_icons\">";
+$icone_maps = "<img src=\"icones/$sub_folder_icons/maps.png\" width=\"$size_icons\">";
+$icone_share = "<img src=\"icones/$sub_folder_icons/share.png\" width=\"$size_icons\">";
+$icone_contact = "<img src=\"icones/$sub_folder_icons/contact.png\" width=\"$size_icons\">";
+$icone_facebook = "<img src=\"icones/$sub_folder_icons/facebook.png\" width=\"$size_icons\">";
+$icone_pinterest = "<img src=\"icones/$sub_folder_icons/pinterest.png\" width=\"$size_icons\">";
+$icone_twitter = "<img src=\"icones/$sub_folder_icons/twitter.png\" width=\"$size_icons\">";
+$icone_instagram = "<img src=\"icones/$sub_folder_icons/instagram.png\" width=\"$size_icons\">";
+$icone_youtube = "<img src=\"icones/$sub_folder_icons/youtube.png\" width=\"$size_icons\">";
+$icone_local = "<img src=\"icones/$sub_folder_icons/local.png\" width=\"$size_icons\">";
+$icone_earth = "<img src=\"icones/$sub_folder_icons/earth.png\" width=\"$size_icons\">";
+$icone_boss = "<img src=\"icones/$sub_folder_icons/boss.png\" width=\"$size_icons\">";
+$icone_ideia = "<img src=\"icones/$sub_folder_icons/ideia.png\" width=\"$size_icons\">";
+$icone_complement = "<img src=\"icones/$sub_folder_icons/complement.png\" width=\"$size_icons\">";
+$icone_point = "<img src=\"icones/$sub_folder_icons/point.png\" width=\"$size_icons\">";
+
+
+// https://material.io/resources/icons/?icon=card_travel&style=baseline
+// https://www.w3schools.com/icons/google_icons_notification.asp
+$icone_learn_engrenagem = "<a href=\"learn/engrenagem\" alt=\"Assista o Vídeo e aprenda como usar\" title=\"Assista o Vídeo e aprenda como usar\"><i class=\"material-icons md-36\">ondemand_video</i></a>";
+$icone_learn_eventos = "<a href=\"learn/eventos\" alt=\"Assista o Vídeo e aprenda como usar\" title=\"Assista o Vídeo e aprenda como usar\"><i class=\"material-icons md-36\">ondemand_video</i></a>";
+$icone_learn_cadastro = "<a href=\"learn/cadastro\" alt=\"Assista o Vídeo e aprenda como usar\" title=\"Assista o Vídeo e aprenda como usar\"><i class=\"material-icons md-36\">ondemand_video</i></a>";
+$icone_learn_caixa = "<a href=\"learn/caixa\" alt=\"Assista o Vídeo e aprenda como usar\" title=\"Assista o Vídeo e aprenda como usar\"><i class=\"material-icons md-36\">ondemand_video</i></a>";
+$icone_learn_aulas = "<a href=\"learn/aulas\" alt=\"Assista o Vídeo e aprenda como usar\" title=\"Assista o Vídeo e aprenda como usar\"><i class=\"material-icons md-36 md-animated\">ondemand_video</i></a>";
+$icone_learn_buscar = "<a href=\"learn/buscar\" alt=\"Assista o Vídeo e aprenda como usar\" title=\"Assista o Vídeo e aprenda como usar\"><i class=\"material-icons md-36\">ondemand_video</i></a>";
+$icone_learn_agenda = "<a href=\"learn/agenda-dia\" alt=\"Assista o Vídeo e aprenda como usar\" title=\"Assista o Vídeo e aprenda como usar\"><i class=\"material-icons md-36\">ondemand_video</i></a>";
+$icone_learn_mensalidade = "<a href=\"learn/mensalidade\" alt=\"Assista o Vídeo e aprenda como usar\" title=\"Assista o Vídeo e aprenda como usar\"><i class=\"material-icons md-36\">ondemand_video</i></a>";
+
+
+
+
+//para manutencao apenas
+//somente se for a pagina site.com.br/icons
+if($conteudo0 == "icons"){
+
+echo "<article>";
+echo "<section>";
+	
+echo "<p>$icone_welcome</p>";
+echo "<p>$icone_user_adm</p>";
+echo "<p>$icone_id</p>";
+echo "<p>$icone_status</p>";
+echo "<p>$icone_person</p>";
+echo "<p>$icone_contract</p>";
+echo "<p>$icone_password</p>";
+echo "<p>$icone_calendar</p>";
+echo "<p>$icone_mail</p>";
+echo "<p>$icone_add</p>";
+echo "<p>$icone_remove</p>";
+echo "<p>$icone_removew</p>";
+echo "<p>$icone_save</p>";
+echo "<p>$icone_savew</p>";
+echo "<p>$icone_edit</p>";
+echo "<p>$icone_editw</p>";
+echo "<p>$icone_opinion</p>";
+echo "<p>$icone_process</p>";
+echo "<p>$icone_whatsapp</p>";
+echo "<p>$icone_telephone</p>";
+echo "<p>$icone_cart</p>";
+echo "<p>$icone_basket</p>";
+echo "<p>$icone_locked</p>";
+echo "<p>$icone_unlocked</p>";
+echo "<p>$icone_unlock</p>";
+echo "<p>$icone_sale</p>";
+echo "<p>$icone_buy</p>";
+echo "<p>$icone_pay</p>";
+echo "<p>$icone_paid</p>";
+echo "<p>$icone_money</p>";
+echo "<p>$icone_payment</p>";
+echo "<p>$icone_customers</p>";
+echo "<p>$icone_administrators</p>";
+echo "<p>$icone_sucess</p>";
+echo "<p>$icone_fail</p>";
+echo "<p>$icone_versus</p>";
+echo "<p>$icone_minus</p>";
+echo "<p>$icone_more</p>";
+echo "<p>$icone_heart</p>";
+echo "<p>$icone_cancel</p>";
+echo "<p>$icone_delete</p>";
+echo "<p>$icone_like</p>";
+echo "<p>$icone_dislike</p>";
+echo "<p>$icone_search</p>";
+echo "<p>$icone_maps</p>";
+echo "<p>$icone_complement</p>";
+echo "<p>$icone_share</p>";
+echo "<p>$icone_contact</p>";
+echo "<p>$icone_facebook</p>";
+echo "<p>$icone_pinterest</p>";
+echo "<p>$icone_instagram</p>";
+echo "<p>$icone_twitter</p>";
+echo "<p>$icone_youtube</p>";
+echo "<p>$icone_local</p>";
+echo "<p>$icone_earth</p>";
+echo "<p>$icone_boss</p>";
+echo "<p>$icone_ideia</p>";
+echo "<p>$icone_point</p>";
+
+echo "</section>";	
+echo "</article>";	
+exit();
+}
+?>
